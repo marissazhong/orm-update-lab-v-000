8cb1962f12b7348eecc299c4ae97b6074428e358
Marissa Zhong <mwz2101@columbia.edu>
2018-07-08 14:13:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,13 +34,17 @@ class Student
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
+    if self.idea
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
   def self.create_table
@@ -60,11 +64,16 @@ class Student
 
     DB[:conn].execute(sql)
   end
-  
+
   def self.create(name:, grade:)
      student = Student.new(name, grade)
      student.save
      student
+   end
+
+   def update
+     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+     DB[:conn].execute(sql, self.name, self.grade, self.id)
    end
 
 end
