da9d80f26f927437f7bea6f9e234870f87a5ff54
Marissa Zhong <mwz2101@columbia.edu>
2018-07-08 14:18:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,8 +65,8 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.create
-    student = Student.new(@name, @grade)
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
     student.save
     student
   end
