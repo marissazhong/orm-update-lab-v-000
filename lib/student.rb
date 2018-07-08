2dfc80d6c913b8f931acbb6a05f7bcdf66b1cc5f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-08 14:22:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,10 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
